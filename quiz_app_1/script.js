a60759f07cb053a270ada39100d3ce81534edc53
ericcarraway <eric@ericcarraway.com>
2014-07-15 16:12:29 -0500

    First Update

---
+++ quiz_app_1/script.js
@@ -0,0 +1,163 @@
+﻿var userStats = {};
+var initializeUserStats = function () {
+    userStats = {
+        currentQuestion: {},
+        currentResponse: "",
+        correctAnswers: 0,
+        incorrectAnswers: 0,
+        totalAnswers: 0,
+        percentage: 0,
+        quizStartTime: 0,
+        quizEndTime: 0,
+        quizCompletionTime: 0,
+        answersPerMinute: 0,
+        speed: 0,
+    };
+};
+initializeUserStats();
+
+
+var questionsToAsk = [];  // global variable
+
+var populateQuestionsToAsk = function () {
+    // treble and/or bass
+    // lines and/or spaces
+    // in the staff and/or below the staff and/or above the staff
+    
+
+
+    var addAllTreblePitchQuestions = function () {
+        questionsToAsk = questionsToAsk.concat(trebleList);
+    };
+
+    var addQuestions = function () {
+        //var userWantsLineOrSpace = "both";
+
+        //for (var i = 0; i < arguments.length; i++) {
+        //    if (arguments[i] === "line") {
+
+        //    }
+        //}
+
+        for (var i in trebleList) {
+            if (trebleList[i].lineOrSpace === "line") {
+                questionsToAsk.push(trebleList[i]);
+            }
+        }
+    };
+
+    var addSpaceTreblePitchQuestions = function () {
+        for (var i = 0; i < trebleList.length; i++) {
+            if (trebleList[i].lineOrSpace === "space") {
+                questionsToAsk.push(trebleList[i]);
+            }
+        }
+    };
+
+    // populate list of cards based on user options
+    // add better control flow for line/space/all/above/in/below
+
+    //addQuestions("lines");
+    addSpaceTreblePitchQuestions();
+
+};
+populateQuestionsToAsk();
+
+
+var setNextQuestion = function () {
+    var questionsToAskIndexRange = questionsToAsk.length - 1;
+    var randomQuestionIndex = Math.floor(Math.random() * questionsToAskIndexRange);
+    userStats.currentQuestion = questionsToAsk[randomQuestionIndex];
+};
+
+var askQuestion = function () {
+    var holder = "";
+    holder += "<img alt='question' src='";
+    holder += userStats.currentQuestion.imgName;
+    holder += "' class='img-rounded' />";
+    document.getElementById("question-img").innerHTML = holder;
+};
+
+var answerSelected = function (pitch) {
+    userStats.currentResponse = pitch;
+    // correct response
+    if (userStats.currentResponse === userStats.currentQuestion.pitch) {
+        userStats.correctAnswers += 1;
+    }
+    // incorrect response
+    else {
+        userStats.incorrectAnswers += 1;
+    }
+    userStats.totalAnswers += 1;
+
+    userStats.percentage = (userStats.correctAnswers / userStats.totalAnswers) * 100;
+    // Math.round rounds to the nearest integer
+    // By multiplying by 10, rounding, and dividing by 10, we round to 1 decimal place
+    userStats.percentage = Math.round(userStats.percentage * 10) / 10;
+    //  TO FIX: inconsistent positioning of percentage element on scoreboard if it's a whole number
+
+    updateScoreboard();
+    setNextQuestion();
+    askQuestion();
+};
+
+var updateScoreboard = function () {
+    document.getElementById("num-right-ans").innerHTML = userStats.correctAnswers;
+    document.getElementById("num-wrong-ans").innerHTML = userStats.incorrectAnswers;
+    document.getElementById("total-answers").innerHTML = userStats.totalAnswers;
+    document.getElementById("percentage").innerHTML = userStats.percentage + " &#37";
+};
+
+var startQuiz = function () {
+    // clear user data and update the scoreboard
+    initializeUserStats();
+    updateScoreboard();
+
+    // decide what the first question will be
+    setNextQuestion();
+
+    // convert the start button to a stop button
+    // will change the function to call to STOPQUIZ
+    var stopButtonHtml = '<button type="button" class="btn btn-danger btn-lg btn-block" onclick="stopQuiz();">stop</button>';
+    document.getElementById("start-stop-button").innerHTML = stopButtonHtml;
+
+    // start the clock
+    userStats.quizStartTime = new Date();
+};
+
+var stopQuiz = function () {
+    // stop the clock
+    userStats.quizEndTime = new Date();
+
+    // convert the stop button to a start button
+    // will change the function to call to STARTQUIZ
+    var startButtonHtml = '<button type="button" class="btn btn-success btn-lg btn-block" onclick="startQuiz();">start</button>';
+    document.getElementById("start-stop-button").innerHTML = startButtonHtml;
+    alertTimeData();
+};
+
+
+var alertTimeData = function() {
+    // compare exerciseEndTime with exerciseStartTime
+    userStats.quizCompletionTime = (userStats.quizEndTime - userStats.quizStartTime) / 1000;  // in seconds
+    // words per minute = (number of CORRECT words in passage ÷ typing time (in seconds)) x 60
+    userStats.answersPerMinute = (userStats.correctAnswers / userStats.quizCompletionTime) * 60;
+    // round to the nearest integer
+    userStats.answersPerMinute = Math.round(userStats.answersPerMinute);
+    userStats.quizCompletionTime = Math.round(userStats.quizCompletionTime);
+    // present data
+    alert("You answered " + userStats.correctAnswers + " questions correctly in " + userStats.quizCompletionTime + " seconds. \n" +
+        "You averaged " + userStats.answersPerMinute + " correct answers per minute \n" +
+        "with " + initializeUserStats.percentage +  "&#37 accuracy."
+        );
+};
+
+
+
+
+
+
+
+
+
+
---
+++ quiz_app_1/treble_pitch_objects.js
@@ -0,0 +1,138 @@
+// array to hold treble objects
+var trebleList = [];
+
+// var treble {} is an object
+// inside of treble are 17 pitches, which are objects
+// each pitch has 4 key:value pairs
+
+var treble = {
+   a4 : {
+        imgName : "treble_a4.png",
+        pitch : "a",
+        lineOrSpace : "space",
+        staffPosition : "in",
+    },
+
+    a5 : {
+        imgName : "treble_a5.png",
+        pitch : "a",
+        lineOrSpace : "line",
+        staffPosition : "above",
+    },
+
+    b4 : {
+        imgName : "treble_b4.png",
+        pitch : "b",
+        lineOrSpace : "line",
+        staffPosition : "in",
+    },
+
+    b5 : {
+        imgName : "treble_b5.png",
+        pitch : "b",
+        lineOrSpace : "space",
+        staffPosition : "above",
+    },
+
+    c4 : {
+        imgName : "treble_c4.png",
+        pitch : "c",
+        lineOrSpace : "line",
+        staffPosition : "below",
+    },
+
+    c5 : {
+        imgName : "treble_c5.png",
+        pitch : "c",
+        lineOrSpace : "space",
+        staffPosition : "in",
+    },
+
+    c6 : {
+        imgName : "treble_c6.png",
+        pitch : "c",
+        lineOrSpace : "line",
+        staffPosition : "above",
+    },
+
+    d4 : {
+        imgName : "treble_d4.png",
+        pitch : "d",
+        lineOrSpace : "space",
+        staffPosition : "below",
+    },
+
+    d5 : {
+        imgName : "treble_d5.png",
+        pitch : "d",
+        lineOrSpace : "line",
+        staffPosition : "in",
+    },
+
+    d6 : {
+        imgName : "treble_d6.png",
+        pitch : "d",
+        lineOrSpace : "space",
+        staffPosition : "above",
+    },
+
+    e4 : {
+        imgName : "treble_e4.png",
+        pitch : "e",
+        lineOrSpace : "line",
+        staffPosition : "in",
+    },
+
+    e5 : {
+        imgName : "treble_e5.png",
+        pitch : "e",
+        lineOrSpace : "space",
+        staffPosition : "in",
+    },
+
+    e6 : {
+        imgName : "treble_e6.png",
+        pitch : "e",
+        lineOrSpace : "line",
+        staffPosition : "above",
+    },
+
+    f4 : {
+        imgName : "treble_f4.png",
+        pitch : "f",
+        lineOrSpace : "space",
+        staffPosition : "in",
+    },
+
+    f5 : {
+        imgName : "treble_f5.png",
+        pitch : "f",
+        lineOrSpace : "line",
+        staffPosition : "in",
+    },
+
+    g4 : {
+        imgName : "treble_g4.png",
+        pitch : "g",
+        lineOrSpace : "line",
+        staffPosition : "in",
+    },
+
+    g5 : {
+        imgName : "treble_g5.png",
+        pitch : "g",
+        lineOrSpace : "line",
+        staffPosition : "above",
+    },
+
+}
+
+var trebleList = [
+    treble.a4, treble.a5,
+    treble.b4, treble.b5,
+    treble.c4, treble.c5, treble.c6,
+    treble.d4, treble.d5, treble.d6,
+    treble.e4, treble.e5, treble.e6,
+    treble.f4, treble.f5,
+    treble.g4, treble.g5,
+];
